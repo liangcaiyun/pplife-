4ac98964164fc79d7cd0d25a609e73f5c6d3dcd0
pengpeng <591455093@qq.com>
2019-12-16 22:17:38 +0800

    first commit

---
+++ js/myQQ.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2019-12-16 21:25:00
+ * @LastEditTime: 2019-12-16 21:46:24
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \qq\js\myQQ.js
+ */
+$(function(){
+    $('#fullpage').fullpage({
+        //显示项目符号
+        navigation: true,
+       //循环滚动
+       loopBottom: true        
+    });
+});
